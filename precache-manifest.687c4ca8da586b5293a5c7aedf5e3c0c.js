4984e6d290c79d3adccfd314a1b4fa2e3e5422c7
dizikhan <44885485+dizikhan@users.noreply.github.com>
2020-07-09 14:50:01 +0900

    Updates

---
+++ precache-manifest.687c4ca8da586b5293a5c7aedf5e3c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53564b1d809aaf940120091343be31d0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "705927a9a6ef452c1d1e",
+    "url": "/movie_app_2020/static/css/main.de34e194.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705927a9a6ef452c1d1e",
+    "url": "/movie_app_2020/static/js/main.4c5a5168.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
